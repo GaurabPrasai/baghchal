64ee7319506874095fa90b2b1d3792c20ccec804
Bipin Thapa <tailung12345678910@gmail.com>
2025-07-11 09:33:33 +0545

    nothing

--- backend/baghchal/consumers.py
+++ backend/baghchal/consumers.py
@@ -1,16 +1,21 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
 import json
-class GameConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
+# from board import process_move, get_new_board
+
+class GameConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        # send a new board layout to the client
         print("websocket connected")
 
-    async def disconnect(self, code):
+    def disconnect(self, message):
         print("websocket closed")
 
-    async def receive(self, text_data):
-        message = json.loads(text_data)['message']
-        print(f"text data received: {message}")
-
+    def receive(self, text_data):
+        move = json.loads(text_data)['message']
+        print(f"text data received: {move}")
+        # >> text data received: {'moveType': 'place', 'currentPlayer': 'goat', 'fromKey': None, 'toKey': '0-3'}
+        # new_board = process_move(move)
+        # print(new_board)
         # echo the message back
-        await self.send(json.dumps(message))
+        # self.send(json.dumps(new_board))
