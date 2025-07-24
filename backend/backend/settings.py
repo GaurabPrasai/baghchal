9be2ee56074a46ddb4feff0923aef9e6926aeef1
Bipin Thapa <tailung12345678910@gmail.com>
2025-07-25 00:28:30 +0545

    create core app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'baghchal',
+    'rest_framework',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -110,6 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'core.User'  # Replace 'baghchal' with your actual app name
 
 
 # Internationalization
