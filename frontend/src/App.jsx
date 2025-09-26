35faff0412cedfad88571569c2f3695b2d84f0a6
Tailung42 <tailung12345678910@gmail.com>
2025-09-27 02:32:26 +0545

    refactor: remove unused import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import UserProfile from "./routes/UserProfile";
 import Home from "./routes/Home";
 import { AuthContext } from "./context/AuthContext";
-import {
-  WebSocketContext,
-  WebSocketProvider,
-} from "./context/WebSocketContext";
+import { WebSocketProvider } from "./context/WebSocketContext";
 import Rules from "./routes/Rules";
 
 const initialAuth = JSON.parse(localStorage.getItem("auth")) || {
