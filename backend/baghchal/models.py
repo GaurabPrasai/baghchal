0f60ac8ac2f6b79db74456b5dde432ad0a80bfac
Tailung <tailung12345678910@gmail.com>
2026-01-15 05:38:05 +0545

    impr: change change field names add str representation

--- backend/baghchal/models.py
+++ backend/baghchal/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 from core.models import User
 
+
 class Game(models.Model):
-    game_id = models.CharField(max_length=8, unique=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_games')
-    player1 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='p1_games')
-    player2 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='p2_games')
-    status = models.CharField(max_length=20, default='waiting')
-    winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='won_games')
+    game_id = models.CharField(primary_key=True, max_length=8, unique=True)
+
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="games_created"
+    )
+
+    goat_player = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="games_as_goat"
+    )
+
+    tiger_player = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="games_as_tiger"
+    )
+
+    winning_player = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="games_won"
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
+
+    move_data = models.JSONField()
+
+
+def __str__(self):
+    return f"Game: {self.game_id}\n\t(Goat: {self.goat_player}, Tiger: {self.tiger_player})"
