7aa1c9d7b0011c6cb0b318738c9c47c9a714f8ab
Tailung <tailung12345678910@gmail.com>
2026-01-15 10:30:09 +0545

    ref: remove created_by and created_at in Game Model

--- backend/baghchal/models.py
+++ backend/baghchal/models.py
@@ -5,9 +5,9 @@ from core.models import User
 class Game(models.Model):
     game_id = models.CharField(primary_key=True, max_length=8, unique=True)
 
-    created_by = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, related_name="games_created"
-    )
+    # created_by = models.ForeignKey(
+    #     User, on_delete=models.SET_NULL, null=True, related_name="games_created"
+    # )
 
     goat_player = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, related_name="games_as_goat"
@@ -20,10 +20,10 @@ class Game(models.Model):
     winning_player = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, related_name="games_won"
     )
+    # ? the object is created after the game is over, so what to store? when player started playing or when the game ended
+    # created_at = models.DateTimeField(auto_now_add=True)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    move_data = models.JSONField()
+    move_data = models.JSONField(default=list)
 
 
 def __str__(self):
